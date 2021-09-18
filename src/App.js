6487bb406cf910e31256f17b1f27eb0fdf326e4b
architprasar <architprasar@gmail.com>
2021-09-18 12:46:11 +0530

    'First'

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Topnav from "./organizer/topnav/topnav";
+function App() {
+  return (
+    <div className="mainapp">
+      <Topnav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/organizer/topnav/topnav.js
@@ -0,0 +1,23 @@
+import React,{ useEffect,useState }  from "react";
+import "../../css/topnav.css";
+
+export default function Topnav() {
+    const [avatar,setAavatar] = useState('n');
+    useEffect(() => {
+        setAavatar('https://avatars.dicebear.com/api/female/pola.svg?mood[]=happy');
+    },[])
+  return (
+    <div className="TopNav">
+      <div className="M_m d_T">
+        <div className="D_T"></div>
+        <div className="D_T"></div>
+        <div className="D_T"></div>
+        <div className="D_T"></div>
+      </div>
+      <div className="N_T">Hi, Louisa</div>
+      <div className="M_m" style={{backgroundImage:'url('+{avatar}+')'}}>
+          <img src={avatar} alt='avatar'/>
+      </div>
+    </div>
+  );
+}
